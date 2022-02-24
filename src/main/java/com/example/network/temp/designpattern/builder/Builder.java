f2a8b3e08e892df212adf964e98e5de251fe64d6
xiongguoliang <xiongguoliang@smartlink.com.cn>
2022-02-24 13:57:40 +0800

    设计模式-创建者模式

---
+++ src/main/java/com/example/network/temp/designpattern/builder/Builder.java
@@ -0,0 +1,8 @@
+package com.example.network.temp.designpattern.builder;
+
+public interface Builder {
+    public void buildDiJi();
+    public void buildQiang();
+    public void buildWuding();
+    public void buildFenShua();
+}
