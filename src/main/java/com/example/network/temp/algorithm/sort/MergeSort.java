71c883e6c254d1e4b7c20f9240358a84985b6656
xiongguoliang <xiongguoliang@smartlink.com.cn>
2022-04-12 15:39:54 +0800

    完善归并排序

--- src/main/java/com/example/network/temp/algorithm/sort/MergeSort.java
+++ src/main/java/com/example/network/temp/algorithm/sort/MergeSort.java
@@ -12,13 +12,18 @@ public class MergeSort {
     public static void sort(int[] a) {
         // 一次性分配空间
         aux = new int[a.length];
+        // 排序数组
         sort(a, 0, a.length - 1);
     }
 
     private static void sort(int[] a, int lo, int hi) {
         // 将数组a[lo..hi]排序
-        if (hi <= lo) return;
+        if (hi <= lo) {
+            return;
+        }
+
         int mid = lo + (hi - lo)/2;
+
         // 将左半边排序
         sort(a, lo, mid);
         // 将右半边排序
@@ -30,22 +35,21 @@ public class MergeSort {
     public static void merge(int[] a, int lo, int mid, int hi) {
         // 将a[lo..mid] 和 a[mid+1..hi] 归并
         int i = lo, j = mid+1;
+
         // 将a[lo..hi]复制到aux[lo..hi]
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
         }
+
         // 归并回到a[lo..hi]
         for (int k = lo; k <= hi; k++) {
             if (i > mid) {
                 a[k] = aux[j++];
-            }
-            else if (j > hi ) {
+            } else if (j > hi ) {
                 a[k] = aux[i++];
-            }
-            else if (SortUtils.less(aux[j], aux[i])) {
+            } else if (SortUtils.less(aux[j], aux[i])) {
                 a[k] = aux[j++];
-            }
-            else {
+            } else {
                 a[k] = aux[i++];
             }
         }
